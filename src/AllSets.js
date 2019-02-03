6b2f1dbcc0a92bcb09af49d639be96a2713052da
Dylan Hall <dhall@campuslabs.com>
2019-02-03 15:21:45 -0500

    remove RNA, gonna split a new branch to resolve the JSON version errors

--- src/AllSets.js
+++ src/AllSets.js
@@ -18,6 +18,7 @@ import ori from "./sets/ORI";
 import xln from "./sets/XLN";
 import zen from "./sets/ZEN";
 import grn from "./sets/GRN";
+//import rna from "./sets/RNA";
 
 export const AllSets = {
   //RNA: rna, //gonna have to migrate all of the parsing to the new json format and update every thing...
